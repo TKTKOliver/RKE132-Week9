68ca471b485d39c198af5dad5cc2051d9eae6d1f
Oliver <oliver.sumin@tktk.ee>
2024-02-04 12:37:50 +0200

    add week 9 code

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Collections.Generic;
+
+static void DisplayDataFromFile(List<string> someList)
+{
+    foreach (string line in someList)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+
